3af25ef053d6e3cf887bf9994a1538d139c3f986
palmerlarsen <pman52@gmail.com>
2014-01-21 07:58:09 -0700

    pushed to online database

---
+++ src/simple/controller/StupidRunner.java
@@ -0,0 +1,15 @@
+package simple.controller;
+
+public class StupidRunner
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
